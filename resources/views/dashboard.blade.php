4779bbfebbd84a48e93cf1c830afb32c061c364a
kareem22T <kotbekareem74@gmail.com>
2024-06-20 02:48:01 +0300

    edist

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -136,7 +136,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
           <button type="button" class="close" style="background: transparent; border: none" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
