92337ea1a12ebf17a6e322816bb1a28ea40df9ff
kareem22T <m.othman@clicksegypt.net>
2024-08-03 23:34:30 +0300

    edis

--- resources/views/amazon.blade.php
+++ resources/views/amazon.blade.php
@@ -286,7 +286,7 @@
                             @case('In stock')
                                 <span class="badge badge-sm bg-gradient-success">In Stock</span>
                                 @break
-                            @elseif(!$product->stock)
+                            @case(!$product->stock)
                               <span class="badge badge-sm bg-gradient-danger">Out of Stock</span>
                             @default
                                 <span class="badge badge-sm bg-gradient-danger">{{$product->stock}}</span>
