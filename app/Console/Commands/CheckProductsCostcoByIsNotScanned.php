d12eb95e1bf22c02bc29a554e0f6cc49dec92d9c
kareem22T <m.othman@clicksegypt.net>
2024-10-21 22:19:25 +0300

    edits

---
+++ app/Console/Commands/CheckProductsCostcoByIsNotScanned.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class CheckProductsCostcoByIsNotScanned extends CheckCostcoProducts
+{
+    protected $signature = 'costco:check_products_not_scaned {offset=0}';
+    protected $description = 'Check products from site 1 for changes in price or stock';
+
+    public function getProducts()
+    {
+        $offset = $this->argument('offset');
+        $products = Product::where('site', 1)->where("isScand", false)->skip($offset)->take(200)->get();
+        return $products;
+    }
+}
